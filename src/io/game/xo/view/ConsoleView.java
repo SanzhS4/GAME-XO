9a1f6ce4d7d4154df1568b2cc9c62cba7a78cc8d
Sereg4 <sanzapovserg@mail.ru>
2020-02-21 21:13:39 +0400

    ConsoleView create

---
+++ src/io/game/xo/view/ConsoleView.java
@@ -0,0 +1,96 @@
+package io.game.xo.view;
+
+import io.game.xo.controllers.CurrentMoveController;
+import io.game.xo.controllers.WinnerController;
+import io.game.xo.controllers.MoveController;
+import io.game.xo.model.Field;
+import io.game.xo.model.Figure;
+import io.game.xo.model.Game;
+
+import io.game.xo.model.exceptions.InvalidPointException;
+import io.game.xo.model.exceptions.AlreadyOccupiedException;
+
+import java.awt.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ConsoleView {
+
+    private final CurrentMoveController currentMoveController = new CurrentMoveController();
+
+    private final WinnerController winnerController = new WinnerController();
+
+    private final MoveController moveController = new MoveController();
+
+    public void show(final Game game) {
+        System.out.format("Название игры: %s\n", game.getName());
+        final Field field = game.getField();
+        for (int x = 0; x < field.getSize(); x++) {
+            if (x != 0)
+                printSeparator();
+            printLine(field, x);
+        }
+
+    }
+
+    public boolean move(final Game game) {
+        final Field field = game.getField();
+        final Figure winner = winnerController.getWinner(field);
+        if (winner != null) {
+            System.out.format("Победитель: %s\n", winner);
+            return false;
+        }
+        final Figure currentFigure = currentMoveController.currentMove(field);
+        if (currentFigure == null) {
+            System.out.println("Победитель не выявлен! Попробуйте сыграть еще раз!");
+            return false;
+        }
+        System.out.format("Ходит игрок: %s\n", currentFigure);
+        final Point point = askPoint();
+        try {
+            moveController.applyFigure(field, point, currentFigure);
+        } catch (final InvalidPointException | AlreadyOccupiedException e) {
+            System.out.println("Координата не верна! Введите еще раз!");
+        }
+        return true;
+    }
+
+    private Point askPoint() {
+        return new Point(askCoordinate("X") - 1, askCoordinate("Y") - 1);
+    }
+
+    private int askCoordinate(final String coordinateName) {
+        System.out.format("Введите координату куда ходите поставить фигуру %s:", coordinateName);
+        final Scanner in = new Scanner(System.in);
+        try {
+            return in.nextInt();
+        } catch (final InputMismatchException e) {
+            System.out.println("Вот это да!");
+            return askCoordinate(coordinateName);
+        }
+    }
+
+    private void printLine(final Field field,
+                           final int x) {
+        for (int y = 0; y < field.getSize(); y++) {
+            if (y != 0)
+                System.out.print("|");
+            System.out.print(" ");
+            final Figure figure;
+            try {
+                figure = field.getFigure(new Point(y, x));
+            } catch (final InvalidPointException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+            System.out.print(figure != null ? figure : " ");
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    private void printSeparator() {
+        System.out.println("~~~~~~~~~~~");
+    }
+
+}
